5553568270f333a789536525af40d91251dcea36
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-06-02 23:23:51 +0300

    add

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,7 +2,7 @@
   const refs = {
     openMenuBtn: document.querySelector(".menu-open-btn"),
     closeMenuBtn: document.querySelector(".menu-close-btn"),
-    menu: document.querySelector(".mob-menu"),
+    menu: document.querySelector(".mob-navigation"),
     body: document.querySelector(".body"),
   };
   refs.openMenuBtn.addEventListener("click", toggleMenu);
